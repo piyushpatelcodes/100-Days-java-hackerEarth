a4d5b4bb81d311f63a6231907aa19837fdd44ea9
Piyush Patel <136020845+piyushpatelcodes@users.noreply.github.com>
2024-06-03 05:29:46 +0530

    Create countDivisors.java

---
+++ countDivisors.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int l = sc.nextInt();
+        int r = sc.nextInt();
+        int k = sc.nextInt();
+        int count=0;
+
+        for(int i=l; i<=r; i++){
+            if(i%k == 0){count++;}
+        }
+        System.out.println(count);
+    }
+}
